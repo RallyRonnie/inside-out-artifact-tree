c96f7c82d2aed027d870aabeeb2f1db9aef0fff5
kcorkan <kcorkan@rallydev.com>
2014-08-28 17:56:20 -0600

    added method to get portfolio item names

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -3,47 +3,58 @@ Ext.define('CustomApp', {
     componentCls: 'app',
     logger: new Rally.technicalservices.Logger(),
     items: [
-        {xtype:'container',itemId:'message_box',tpl:'Hello, <tpl>{_refObjectName}</tpl>'},
         {xtype:'container',itemId:'display_box'},
         {xtype:'tsinfolink'}
     ],
     launch: function() {
-        this.down('#message_box').update(this.getContext().getUser());
         
         var m_name = 'Defect',
         m_fields = ['Name','State'];
         
-        this._loadAStoreWithAPromise(m_name, m_fields).then({
+        this._fetchPortfolioNames().then({
             scope: this,
-            success: function(store){
-                this.down('#display_box').add({
-                    xtype: 'rallygrid',
-                    store: store,
-                    columnCfgs: m_fields
-                });
+            success: function(){
+                console.log(this.sModelNames)
             },
-            failure: function(error_message){
-                alert(error_message);
+            failure: function(error_msg){
+                alert(error_msg);
+                this.logger.log(error_msg);
             }
         });
-    },
-    _loadAStoreWithAPromise: function(model_name, model_fields){
+        
+    }, 
+    _fetchPortfolioNames: function(){
         var deferred = Ext.create('Deft.Deferred');
         
-        var defectStore = Ext.create('Rally.data.wsapi.Store', {
-            model: model_name,
-            fetch: model_fields,
+        var typeStore = Ext.create('Rally.data.wsapi.Store', {
             autoLoad: true,
-            listeners: {
-                load: function(store, records, successful) {
-                    if (successful){
-                        deferred.resolve(store);
+            model: 'TypeDefinition',
+            sorters: [{
+              property: 'Ordinal',
+              direction: 'ASC'
+            }],
+            filters: [{
+              property: 'Parent.Name',
+              operator: '=',
+              value: 'Portfolio Item'
+            }, {
+              property: 'Creatable',
+              operator: '=',
+              value: true
+            }],
+            listeners:  {
+                scope: this,
+                load: function(store, records, success){
+                    if (success) {
+                        var sModelNames = _.map(records, function (rec) { return rec.get('TypePath'); });
+                        deferred.resolve(sModelNames);
                     } else {
-                        deferred.reject('Failed to load store for model [' + model_name + '] and fields [' + model_fields.join(',') + ']');
+                        deferred.reject('Error loading portofolio item names.');
                     }
-                }
-            }
+               }
+           }
         });
         return deferred.promise;
     }
+
 });
